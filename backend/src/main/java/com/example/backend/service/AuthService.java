35db3770d26ac3793ce02f631bc8aceeaf5428c4
ddiddokr <ddiddokr1@naver.com>
2025-10-22 16:52:26 +0900

    temp: save local AuthService changes before checkout

--- backend/src/main/java/com/example/backend/service/AuthService.java
+++ backend/src/main/java/com/example/backend/service/AuthService.java
@@ -1,6 +1,5 @@
 package com.example.backend.service;
 
-<<<<<<< HEAD
 import com.example.backend.dto.*;
 import com.example.backend.entity.Provider;
 import com.example.backend.entity.User;
@@ -9,7 +8,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 import com.example.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
