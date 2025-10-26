eac4f609c484c3e63dcbbdac7db7bb724b0fabd8
Seungjoon Lee <sg15400@gmail.com>
2025-10-26 13:59:57 +0900

    photo entity생성, 후기에 photo업로드)(api test 안해봄)

--- backend/src/main/java/com/example/backend/controller/S3Controller.java
+++ backend/src/main/java/com/example/backend/controller/S3Controller.java
@@ -2,6 +2,7 @@ package com.example.backend.controller;
 
 import com.example.backend.entity.User;
 import com.example.backend.service.AuthenticationService;
+import com.example.backend.service.PhotoService;
 import com.example.backend.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import java.util.Map;
 public class S3Controller {
     final private AuthenticationService authenticationService;
     final private S3Service s3Service;
+    final private PhotoService photoService;
     @PostMapping("/profileImage")
     public ResponseEntity<Map<String, String>> uploadProfileImage(@RequestPart("file") MultipartFile file) {
         User currentUser = authenticationService.getCurrentUser();
@@ -24,13 +26,14 @@ public class S3Controller {
         currentUser.setProfileImageUrl(url);
         return ResponseEntity.ok(Map.of("url", url));
     }
-    //여행후기때 추가 예정?
-//    @PostMapping("/image")
-//    public ResponseEntity<Map<String, String>> uploadImage(@RequestPart("file") MultipartFile file) {
-//        User currentUser = authenticationService.getCurrentUser();
-//        String currentName = currentUser.getUsername();
-//        String url = s3Service.uploadFile(file, currentName+"/");
-//        currentUser.setProfileImageUrl(url);
-//        return ResponseEntity.ok(Map.of("url", url));
-//    }
+    @PostMapping("{reviewId}/image")
+    public ResponseEntity<Map<String, String>> uploadImage(
+            @RequestParam Long reviewId,
+            @RequestPart("file") MultipartFile file) {
+        User currentUser = authenticationService.getCurrentUser();
+        String currentName = currentUser.getUsername();
+        String url = s3Service.uploadFile(file, currentName+"/review/"+reviewId+"/");
+        photoService.savePhotoToReview(reviewId, url);
+        return ResponseEntity.ok(Map.of("url", url));
+    }
 }
