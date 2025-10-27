6bf36ef70baef0ef3025d5193585f0584ddd33fb
swkil <0826monica@naver.com>
2025-10-27 15:48:00 +0900

    여행 후기 ui 변경

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -89,7 +89,7 @@ export default function ProfilePage() {
     };
 
     return (
-        <div className="flex items-center justify-center min-h-screen p-4 sm:p-6 lg:p-8 font-inter">
+        <div className="flex items-center justify-center min-h-screen p-4 sm:p-6 lg:p-8">
             {isEditing ? (
                 <ProfileEditForm
                     currentProfile={profileData}
